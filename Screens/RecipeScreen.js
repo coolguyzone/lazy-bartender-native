59f2d60159f5ab7cd6bedb17e7119883b237a748
Alex Krawiec <alexanderkrawiec@gmail.com>
2024-04-04 14:36:50 -0700

    Start coding out ingredients blade

--- Screens/RecipeScreen.js
+++ Screens/RecipeScreen.js
@@ -13,15 +13,82 @@ function RecipeScreen() {
     <>
       <ScrollView style={styles.scrollView}>
         <RecipeHeader />
-        <View style={styles.recipeImage}><Text>Recipe Image</Text></View>
+        <View style={styles.recipeImage}>
+          <Text>Recipe Image</Text>
+        </View>
         <View style={styles.recipeH1BladeContainer}>
-        <Text style={styles.recipeH1BladeTitle}>Cocktail Name</Text>
-       
-        </View>  
+          <Text style={styles.recipeH1BladeTitle}>Cocktail Name</Text>
+        </View>
         <View style={styles.strengthBarContainer}>
           <Text style={styles.strengthBarCopy}>Strength:</Text>
         </View>
-        <View style={styles.divider}></View>     
+        <View style={styles.divider}></View>
+        <View style={styles.ingredientBladeContainer}>
+          <View style={styles.ingredientBladeRow}>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeHeader}>Ingredients:</Text>
+            </View>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeHeader}>Qty</Text>
+            </View>
+            <View
+              style={[
+                styles.ingredientBladeColumn,
+                styles.ingredientBladeFinalColumn,
+              ]}
+            >
+              <Text style={styles.ingredientBladeHeader}>In Bar</Text>
+            </View>
+          </View>
+          <View style={styles.ingredientBladeRow}>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeText}>Rye Whiskey</Text>
+            </View>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeText}>2 Ounces</Text>
+            </View>
+            <View
+              style={[
+                styles.ingredientBladeColumn,
+                styles.ingredientBladeFinalColumn,
+              ]}
+            >
+              <View style={styles.ingredientBladeCircle}></View>
+            </View>
+          </View>
+          <View style={styles.ingredientBladeRow}>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeText}>Sweet Vermouth</Text>
+            </View>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeText}>1 Ounce</Text>
+            </View>
+            <View
+              style={[
+                styles.ingredientBladeColumn,
+                styles.ingredientBladeFinalColumn,
+              ]}
+            >
+              <View style={styles.ingredientBladeCircle}></View>
+            </View>
+          </View>
+          <View style={styles.ingredientBladeRow}>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeText}>Aromatic Bitters</Text>
+            </View>
+            <View style={styles.ingredientBladeColumn}>
+              <Text style={styles.ingredientBladeText}>2-3 Dashes</Text>
+            </View>
+            <View
+              style={[
+                styles.ingredientBladeColumn,
+                styles.ingredientBladeFinalColumn,
+              ]}
+            >
+              <View style={styles.ingredientBladeCircle}></View>
+            </View>
+          </View>
+        </View>
       </ScrollView>
       <Footer />
     </>
@@ -42,9 +109,9 @@ const styles = StyleSheet.create({
     color: GlobalStyles.colors.robRoy100,
   },
   recipeH1BladeContainer: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",  
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   recipeH1BladeTitle: {
     fontSize: 24,
@@ -55,12 +122,47 @@ const styles = StyleSheet.create({
   strengthBarCopy: {
     color: GlobalStyles.colors.robRoy100,
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginLeft: 16,
   },
   divider: {
     borderColor: GlobalStyles.colors.robRoy100,
     borderBottomWidth: 1,
     marginVertical: 24,
+  },
+  ingredientBladeContainer: {
+    paddingHorizontal: 24,
+  },
+  ingredientBladeRow: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginBottom: 24,
+  },
+  ingredientBladeColumn: {
+    flexDirection: "column",
+    flex: 1,
+  },
+  ingredientBladeFinalColumn: {
+    flex: 0.5,
+  },
+  ingredientBladeCircle: {
+    flex: 0.5,
+  },
+  ingredientBladeHeader: {
+    color: GlobalStyles.colors.robRoy100,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  ingredientBladeText: {
+    color: GlobalStyles.colors.robRoy100,
+  },
+  ingredientBladeCircle: {
+    marginLeft: 14,
+    height: 18,
+    width: 18,
+    borderColor: GlobalStyles.colors.robRoy100,
+    // backgroundColor: GlobalStyles.colors.tonysPink300,
+    borderWidth: 1,
+    borderRadius: 100,
   },
 });
